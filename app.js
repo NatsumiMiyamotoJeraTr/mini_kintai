f74e0c838462dba3351f7d1388e9bb4aa57d3791
NatsumiMiyamotoJeraTr <Jera.hq.devtr.mem010@outlook.jp>
2026-02-18 17:34:41 +0900

    create:BE: swaggerUIでopenapiドキュメントを作成

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const express = require('express');
-const knex = require('./knex');
+const knex = require('./db/knex');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
-const { initUser } = require('./user/index');
-const { initAttendance } = require('./attendance/index');
+const { initUser } = require('./src/user/index');
+const { initAttendance } = require('./src/attendance/index');
+const { authMiddleware } = require('./src/middleware/auth');
 
 function buildApp() {
   const app = express();
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  app.use(authMiddleware); // リクエストにuser_idをつけくわえた状態で、全エンドポイント実行
+
+  // OpenAPI ドキュメント設定
+  const openApiPath = './openapi.yaml';
+  const swaggerDocument = YAML.load(openApiPath);
+
+  // Swagger UI を /api-docs エンドポイント で公開
+  app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, {
+      swaggerOptions: {
+        url: '/openapi.yaml',
+      },
+    })
+  );
+
+  // openapi.yaml を静的ファイルで公開
+  app.get('/openapi.yaml', (req, res) => {
+    res.sendFile(openApiPath);
+  });
 
   const userController = initUser(knex);
   const attendanceController = initAttendance(knex);
@@ -28,8 +53,8 @@ function buildApp() {
 
   app.get('/attendances', attendanceController.list);
   app.get('/attendances/:id', validateIdMiddleware, attendanceController.find);
-  app.post('/attendances/clock-in', attendanceController.create);
-  app.post('/attendances/clock-out', attendanceController.create);
+  app.post('/attendances/clock-in', attendanceController.clockIn);
+  app.post('/attendances/clock-out', attendanceController.clockOut);
   app.patch(
     '/attendances/:id',
     validateIdMiddleware,
