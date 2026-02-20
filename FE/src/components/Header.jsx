e5fb0cacc87bb7be2e3c114b7d7b70c0414258c4
NatsumiMiyamotoJeraTr <Jera.hq.devtr.mem010@outlook.jp>
2026-02-20 10:05:54 +0900

    feat: FE: headerタイトルにtopページへのリンク追加

--- FE/src/components/Header.jsx
+++ FE/src/components/Header.jsx
@@ -1,3 +1,18 @@
+import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthUserContext';
+
 export const Header = ({ innerText }) => {
+  const { user } = useAuth();
+
+  if (user) {
+    return (
+      <header>
+        <Link to="/top" style={{ textDecoration: 'none', color: 'inherit' }}>
+          {innerText}
+        </Link>
+      </header>
+    );
+  }
+
   return <header>{innerText}</header>;
 };
